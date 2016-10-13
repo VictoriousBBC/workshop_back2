dc4d34c716705171727625aa702066c837c9c028
Francois Martel <francois.martel@hec.edu>
2016-10-13 17:06:47 +0200

    almost end of WB3

--- Gemfile
+++ Gemfile
@@ -1,10 +1,15 @@
-source 'https://rubygems.org'
-
-
+source 'http://rubygems.org'
+ruby'2.2.4'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+
+gem 'sqlite3', group: :development
+
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -44,3 +49,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "twitter-bootstrap-rails"
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+Show.create(
+	name: "Mon premier Show",
+	loation: "Salle Pleyel",
+	description: "Concert blabalbla",
+	capacity: 500,
+	price: 30,
+	image: "http://www.sallepleyel.fr/img/visuel/diaporama/salle_concert_scene.jpg",
+	date: "2016-10-30"
+)
+
+Show.create(
+	name: "Sébastien Tellier",
+	location: "Nouveau Casino",
+	description: "L'Aventura",
+	capacity: 500,
+	price: 36,
+	image: "http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg",
+	date: "2016-10-19"
+)
+
+Show.create(
+	name: "Francois Martel",
+	location: "24 rue Robert Fleury, 75015 Paris",
+	description: "Grosse teuf pour mon anniversaire",
+	capacity: 40,
+	price: 5,
+	date: "2016-10-28",
+)
+
+	# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
